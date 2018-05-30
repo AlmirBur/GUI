61e169fc888aa6b99b889151c6d429502525204c
AlmirBur <31883981+AlmirBur@users.noreply.github.com>
2018-05-30 15:42:18 +0300

    удалён конструктор класса Field

--- src/othello/model/Field.java
+++ src/othello/model/Field.java
@@ -6,10 +6,6 @@ import java.util.List;
 public class Field {
     private Cell[][] cells = new Cell[8][8];
 
-    public Field() {
-        restart();
-    }
-
     public void restart() {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
